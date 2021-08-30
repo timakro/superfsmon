31704a1cbea080ca1fa304e68b3d53cb16ea0aaa
Daniel Pittman <daniel@rimspace.net>
2021-08-30 11:30:28 +0200

    depend on `supervisor-win` on win32
    `supervisor` doesn't run on Windows, only POSIX systems.
    The `supervisor-win` package extends it to support win32.
    
    This adjusts the dependencies to use the platform-specific version of
    supervisor on Windows.  Since `supervisor-win` installs all of
    `supervisor` the existing code works unmodified.
    
    The alternative would be to drop the dependency entirely, which didn't
    seem like the right choice.

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup
 
 setup(name='superfsmon',
-      version='1.2.0',
+      version='1.2.1',
       license='MIT',
       description='Supervisor plugin to watch a directory and restart '
                   'programs on changes',
       author='Tim Schumacher',
       author_email='tim@timakro.de',
       url='https://github.com/timakro/superfsmon',
-      install_requires=['supervisor', 'watchdog'],
+      install_requires=[
+          'supervisor >= 4.0.0     ; sys_platform != "win32"',
+          'supervisor-win >= 4.0.0 ; sys_platform == "win32"',
+          'watchdog',
+      ],
       packages=['superfsmon'],
       entry_points={
           'console_scripts': ['superfsmon=superfsmon:main']
