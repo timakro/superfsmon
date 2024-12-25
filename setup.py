974e05d574abaeb2b24e0c44d5c26de5b19de713
Tim Schumacher <tim@tschumacher.net>
2024-12-25 19:21:23 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='superfsmon',
-      version='1.2.2',
+      version='1.2.3',
       license='MIT',
       description='Supervisor plugin to watch a directory and restart '
                   'programs on changes',
       author='Tim Schumacher',
-      author_email='tim@timakro.de',
+      author_email='tim@tschumacher.net',
       url='https://github.com/timakro/superfsmon',
       install_requires=[
           'supervisor >= 4.0.0     ; sys_platform != "win32"',
